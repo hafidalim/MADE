959da10cd6bb5c592af618b5f7ea6123be2ea625
Hafid Ali Mustaqim <hafidd456@gmail.com>
2019-09-29 01:04:41 +0700

    intent activity

--- MyIntentApp/app/src/main/java/com/hafid/myapplication/MainActivity.java
+++ MyIntentApp/app/src/main/java/com/hafid/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.hafid.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnPindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPindah = findViewById(R.id.btnMoveActivity);
+        btnPindah.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnMoveActivity:
+                Intent intent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
