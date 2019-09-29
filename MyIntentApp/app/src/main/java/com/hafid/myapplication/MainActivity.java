9e05bb50ec75297c472d5e43b78f2caaeb80426b
Hafid Ali Mustaqim <hafidd456@gmail.com>
2019-09-29 10:43:07 +0700

    intent putextra

--- MyIntentApp/app/src/main/java/com/hafid/myapplication/MainActivity.java
+++ MyIntentApp/app/src/main/java/com/hafid/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnPindah;
+    Button btnPindah, btnMoveData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnPindah = findViewById(R.id.btnMoveActivity);
         btnPindah.setOnClickListener(this);
+
+        btnMoveData = findViewById(R.id.btnmoveData);
+        btnMoveData.setOnClickListener(this);
+
     }
 
     @Override
@@ -26,6 +30,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent = new Intent(MainActivity.this, MoveActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.btnmoveData:
+
+                //menggunakan put extra untuk mengirim object
+                Intent moveData= new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveData.putExtra(MoveWithDataActivity.EXTRA_NAME, "Hafid Ali Mustaqim");
+                moveData.putExtra(MoveWithDataActivity.EXTRA_AGE, 18);
+                startActivity(moveData);
+
+            break;
         }
     }
 }
